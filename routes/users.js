4827f05b7f12c38da798347eff16b25ff69c5922
Vivek Singh <viveksingh27795@gmail.com>
2022-02-19 22:57:49 +0530

    routes

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET user listing. */
 router.get('/1', function(req, res, next) {
   res.send('just text');
 });
